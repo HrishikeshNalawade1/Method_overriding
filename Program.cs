4dfd5e9e150006c4d51990134887e820740530a2
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-23 10:33:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace method_overiding
+{
+    class Dog
+    {
+        public virtual void  walk()
+        {
+            Console.WriteLine("Dog is walking");
+        }
+    }
+    class cat:Dog
+    {
+        public override void walk()
+        {
+            Console.WriteLine("Cat is walking");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cat obj = new cat();
+            //obj.walk();
+            Dog obj = new Dog();
+            obj.walk();
+            obj = new cat();
+            obj.walk();
+        }
+        
+    }
+}
